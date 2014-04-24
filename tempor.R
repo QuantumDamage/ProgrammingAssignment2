40ca0a8954079d0565404515cd2138dbcd72d9c9
Damian Melniczuk <damian.melniczuk@gmail.com>
2014-04-24 08:24:52 +0200

    testing case

---
+++ tempor.R
@@ -0,0 +1,8 @@
+testowaMatryca <- matrix(c(4,5,6,3,3,2,1,1,0),3,3)
+source("cachematrix.R")
+rozwiazanaTestowaMatryca <- solve(testowaMatryca)
+keszowaTestowaMatryca <- makeCacheMatrix(testowaMatryca)
+wynik <- cacheSolve(keszowaTestowaMatryca)
+identical(rozwiazanaTestowaMatryca,wynik)
+wynik <- cacheSolve(keszowaTestowaMatryca)
+identical(rozwiazanaTestowaMatryca,wynik)
